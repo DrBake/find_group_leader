7686845f12ebc5da881fc519fb9007a545955091
DrBake <mangilenox@gmail.com>
2023-03-16 15:12:09 -0700

    optimize the code

--- leader.c
+++ leader.c
@@ -7,7 +7,7 @@
  *       then the member is chosen from the array
  *       if number chosen == \0 then a number number is chosen.
  *
- *retun: 0
+ *return: 0
  */
 
 int main (void){
@@ -18,7 +18,6 @@ int main (void){
 	char *member[] = {"member_1", "member_2", "member_3", "member_4", "member_5", "member_6", "member_7", "\0"};
 
 	srand(time(NULL));
-
 
 	number_of_members = sizeof(*member)/sizeof(*member[0]);
 	random_number = rand() % number_of_members;
