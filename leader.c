f7d8d73a736ef9236c4e20f40324974287778dc3
DrBake <mangilenox@gmail.com>
2023-03-16 14:42:04 -0700

    removed segmentation fault

--- leader.c
+++ leader.c
@@ -7,16 +7,20 @@ int main (void){
 	int random_number;
 	int number_of_members;
 	
-	char *member[] = {"member_1", "member_2", "member_3", "member_4", "member_5", "member_6", "member_7"};
+	char *member[] = {"member_1", "member_2", "member_3", "member_4", "member_5", "member_6", "member_7", "\0"};
 
 	srand(time(NULL));
 
 
-	number_of_members = sizeof(*member)/sizeof(*member[0])
+	number_of_members = sizeof(*member)/sizeof(*member[0]);
 	
 	random_number = rand() % number_of_members;
 
-	printf("%s will lead the discussion \n", *member[random_number]);
+
+	while (random_number == number_of_members - 1)
+		random_number = rand() % number_of_members;	
+
+	printf("%s will lead the discussion \n", member[random_number]);
 
 	return 0;
 }
