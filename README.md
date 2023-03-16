904a65bcfc2fd1a47629beddbb90c7c25d02bc69
DrBake <mangilenox@gmail.com>
2023-03-17 00:03:13 +0300

    algorith to randomly choose a person

---
+++ README.md
@@ -0,0 +1,1 @@
+An algorithm that will randomly choose a member of a group to lead a discussion in a peer learning session.
---
+++ leader.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (void){
+	
+	int random_number;
+	int number_of_members;
+	
+	char *member[] = {"member_1", "member_2", "member_3", "member_4", "member_5", "member_6", "member_7"};
+
+	srand(time(NULL));
+
+
+	number_of_members = sizeof(*member)/sizeof(*member[0])
+	
+	random_number = rand() % number_of_members;
+
+	printf("%s will lead the discussion \n", *member[random_number]);
+
+	return 0;
+}
